d563c289baa1dd9607a6b6acea3be2154b62f666
Preeti Dewani <preetidewani@Preetis-MacBook-Pro.local>
2024-05-14 11:15:03 +0530

    main: send data to otel backend via vmagent

---
+++ push-using-agent/vmagent/env.sh
@@ -0,0 +1,4 @@
+export OTLP_COLLECTOR_HOST_ADDRESS='http://vmagent:8429'
+export OTLP_COLLECTOR_RESOURCE_PATH='opentelemetry/api/v1/push'
+export REMOTE_WRITE_URL='http://victoriametrics:8429/api/v1/write'
+
